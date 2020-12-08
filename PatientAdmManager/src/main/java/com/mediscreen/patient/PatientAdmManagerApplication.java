0d19a00ec55f325d86efe6014b4357ea6058eeeb
Thierry Schreiner <Thierry.Schreiner@hotmail.fr>
2020-12-08 21:17:37 +0100

    refactor: rename PatientManager as PatientAdmManager

--- PatientAdmManager/src/main/java/com/mediscreen/patient/PatientAdmManagerApplication.java
+++ PatientAdmManager/src/main/java/com/mediscreen/patient/PatientAdmManagerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PatientManagerApplication {
+public class PatientAdmManagerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PatientManagerApplication.class, args);
+		SpringApplication.run(PatientAdmManagerApplication.class, args);
 	}
 
 }
